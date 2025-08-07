c339d0c92d143eb341864c349ac16c03873e35d6
Steven Rogers <s.hunter.rogers@gmail.com>
2025-08-07 01:05:18 -0400

    tweaked inheritance, need to fix testing for card class. used a lot of __init__.py and -m

--- CardUtils/Card.py
+++ CardUtils/Card.py
@@ -1,3 +1,6 @@
+from CardUtils.Ace import Ace
+from CardUtils.FaceCard import FaceCard
+
 """The card class allows for customization of a deck """
 
 class Card:
@@ -7,5 +10,30 @@ class Card:
         self.suit = suit
         self.is_face_up = is_face_up
 
+    def __str__(self):
+        print(f"{self.face_value} of {self.suit}: value: {self.actual_value}")
+
+    @classmethod
+    def create(cls, face_value, actual_value, suit, is_face_up=True):
+        face_value_str = str(face_value).upper()
+
+        if face_value_str == "A":
+            return Ace(suit, is_face_up)
+        elif face_value_str in ("J", "Q", "K"):
+            return FaceCard(face_value_str, suit, is_face_up)
+        else:
+            try:
+                value = int(face_value_str)
+                return cls(face_value_str, value, suit, is_face_up)
+            except ValueError:
+                raise ValueError(f"Invalid face value: {face_value}")
+
+
+ace_diamonds = Card.create("A", "Diamonds")
+ace_clubs = Card.create("A", "Clubs")
+jack_diamonds = Card.create("J", "Diamonds")
+king_diamonds = Card.create("K", "Diamonds")
+
+print(ace_diamonds, ace_clubs, jack_diamonds, king_diamonds, sep="\n")
 
 
